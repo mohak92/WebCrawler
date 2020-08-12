5e4307ea1649cdd67fbc7f8785a81c013136ae91
mohak92 <tamhane.mohak@gmail.com>
2020-08-12 15:38:30 -0400

    Main class for web crawler

--- src/com/mohak/webCrawler/App.java
+++ src/com/mohak/webCrawler/App.java
@@ -3,6 +3,12 @@ package com.mohak.webCrawler;
 public class App {
 
 	public static void main(String[] args) {
+		
+		WebCrawler webCrawler = new WebCrawler();
+		
+		String rootUrl = "https://www.bbc.com";
+		
+		webCrawler.discoverWeb(rootUrl);
 
 	}
 
