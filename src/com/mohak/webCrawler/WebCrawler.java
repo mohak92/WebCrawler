2dae4cc0e4879b315b02add0c3da6adfe2ca831c
mohak92 <tamhane.mohak@gmail.com>
2020-08-12 11:15:11 -0400

    read url

--- src/com/mohak/webCrawler/WebCrawler.java
+++ src/com/mohak/webCrawler/WebCrawler.java
@@ -1,5 +1,8 @@
 package com.mohak.webCrawler;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +16,39 @@ public class WebCrawler {
 	public WebCrawler() {
 		this.queue =  new LinkedList<>();
 		this.discoveredWebsiteList = new ArrayList<>();
+	}
+	
+	public void discoverWeb(String root) {
+		
+		this.queue.add(root);
+		this.discoveredWebsiteList.add(root);
+		
+		while(!queue.isEmpty()) {
+			String v = this.queue.remove();
+			String rawHtml = readURL(v); 
+		}
+	}
+
+	private String readURL(String v) {
+		
+		String rawHtml = "";
+		
+		try {
+		
+			URL url = new URL(v);
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			String inputLine = "";
+			
+			while((inputLine =  in.readLine()) != null) {
+				rawHtml += inputLine;
+			}
+			
+			in.close();
+		} catch (Exception e) {
+			
+		}
+		return rawHtml;
 	}
 
 }
