74e1f0ff286b5ab610f720d7b0d8cd94c4c1e5ec
mohak92 <tamhane.mohak@gmail.com>
2020-08-12 08:12:11 -0400

    fields

--- src/com/mohak/webCrawler/WebCrawler.java
+++ src/com/mohak/webCrawler/WebCrawler.java
@@ -1,5 +1,18 @@
 package com.mohak.webCrawler;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class WebCrawler {
+	
+	private Queue<String> queue;
+	private List<String> discoveredWebsiteList;
+	
+	public WebCrawler() {
+		this.queue =  new LinkedList<>();
+		this.discoveredWebsiteList = new ArrayList<>();
+	}
 
 }
